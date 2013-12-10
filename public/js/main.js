01146246c64207b92f532d67e13932fb14a5d0d0
Simon Bugert <simon.bugert@googlemail.com>
2013-12-10 15:37:47 +0100

    Fixed post request for sorting game

--- public/js/main.js
+++ public/js/main.js
@@ -7,7 +7,7 @@ $(function() {
     var serialized = $("#buildings").sortable("serialize", {
       key: "buildingid"
     });
-    $.post('/sortbuildings', serialized, function(data) {
+    $.post('/validateorder', serialized, function(data) {
       $('#content').append('<div data-dismiss="alert" class="alert alert-info alert-dismissable">'
         + data + '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button></div>');
     });
