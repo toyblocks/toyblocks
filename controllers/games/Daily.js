1f83da977a0cb1ac99db207a2639dbce533efe62
Steven Reynolds <reynolds.tud@gmail.com>
2014-01-27 18:18:32 +0100

    added first pages for DailyChallenge

---
+++ controllers/games/Daily.js
@@ -0,0 +1,131 @@
+var GamesController = require('../Games'),
+attributeModel = require('../../models/Attribute');
+
+module.exports = function () {
+
+};
+
+module.exports.prototype = GamesController.prototype.extend({
+  name: 'daily',
+
+  indexAction: function() {
+    var _this = this;
+
+    // TODO this.mongodb.collection('players').find( hasPlayedDaily: true).count()
+    var currentGamesPlayed = 120,
+    // TODO this.mongodb.collection('players').count()
+    maxGamesPlayed = 200, 
+    percentGamesPlayed = (currentGamesPlayed*100 / maxGamesPlayed );
+
+    _this.view.render( {
+      title: 'Daily Challenge',
+      gamesPlayed: percentGamesPlayed,
+      hasPlayedTodaysDaily: true
+    });
+  },
+
+  generateDailyGame: function() {
+    var _this = this;
+    var missing = _this.mongodb.collection('missingparts_games').find({}).toArray(function(err, data) {
+        for (var i = data.length - 1; i >= 0; i--) {
+        console.log("missingparts_games - " + data[i].title);  
+    };
+          
+    }),
+    sorting = _this.mongodb.collection('sorting_games').find().toArray(function(err, data) {
+      for (var i = data.length - 1; i >= 0; i--) {
+      console.log("sorting_games - " + data[i].title);  
+    };
+      
+    });
+
+    // TODO: render game at 0:00, save in mongodb
+    // TODO: set hasPlayedDaily for all players to false
+    // TODO: also insert date to mongodb
+    // TODO: create a new leaderboard
+  },
+
+  gameAction: function() {
+    var _this = this;
+
+    // this counts the available games, takes a random of each and sends it back to the client
+    // TODO: create a db entry for daily game and send the entry back
+    _this.generateDailyGame();
+    var games;
+    _this.mongodb.collection('missingparts_games').find().count(function (e, count1) {
+      var random = Math.random() * count1;
+      _this.mongodb.collection('missingparts_games').find().limit(-1).skip(random).next(function (err, ele1) {
+        _this.mongodb.collection('sorting_games').find().count(function (e, count2) {
+          random = Math.random() * count2;
+          _this.mongodb.collection('sorting_games').find().limit(-1).skip(random).next(function (err, ele2) {
+            games = [{
+              id: ele1._id,
+              type: "missing",
+              title: ele1.title
+            },{
+              id: ele2._id,
+              type: "sorting",
+              title: ele2.title
+            }];
+
+            // log debug info
+            console.log(ele1);
+            console.log("games");
+            console.log(games);
+
+            // send data back to client
+            _this.view.render({
+              games: games
+            })
+          });
+        });
+      });
+    });
+  },
+
+  leaderboardAction: function () {
+    var _this = this;
+
+    //TODO: get proper player data from mongodb
+    var players2 = [];
+    var players = [ {
+      pos: 1,
+      name: 'FayeValentine',
+      score: '1337',
+      logo: 'none'},{
+      pos: 2,
+      name: 'Edward',
+      score: '1200',
+      logo: 'none'}, {
+      pos: 3,
+      name: 'SpikeSpiegel',
+      score: '1111',
+      logo: 'none'},{
+      pos: 4,
+      name: 'Ein',
+      score: '734',
+      logo: 'none'},{
+      pos: 5,
+      name: 'Jet Black',
+      score: '234',
+      logo: 'none'
+    }];
+
+    // TODO: get date from mongodb
+    var d = new Date();
+    var game = {
+      year: d.getFullYear(),
+      month: d.getMonth()+1,
+      day: d.getDate()
+    };
+
+    _this.view.render({
+      game: game,
+      players: players
+    })
+  },
+
+  renderGame: function(game, renderCallback) {
+    // no rendering here booooy
+  }
+});
