293d72b067a7c0695f2475b2113ccf26f65d683c
Steven Reynolds <reynolds.tud@gmail.com>
2014-02-03 14:21:21 +0100

    SublimeLinter fixes, more comments and bugfixes

--- controllers/games/Missing.js
+++ controllers/games/Missing.js
@@ -1,15 +1,19 @@
-var GamesController = require('../Games'),
-attributeModel = require('../../models/Attribute');
+var GamesController = require('../Games');
 
-module.exports = function () {
-
-};
+module.exports = function () {};
 
 module.exports.prototype = GamesController.prototype.extend({
   name: 'missing',
 
-  // overview of games
+  /**
+  *  indexAction() answers GET requests
+  *  for the overview of missingpart games
+  *
+  * @return <String> title Title of indexpage
+  * @return <Array> data 
+  */
   indexAction: function() {
+    'use strict';
     var _this = this;
     this.mongodb
     .collection('missingparts_games')
@@ -22,9 +26,17 @@ module.exports.prototype = GamesController.prototype.extend({
     });
   },
 
-  // actual missingparts game
-  // @returns game information and game images
+  /**
+  *  gameAction() answers GET requests
+  *  for actual missingparts game
+  *
+  * @return <String> title
+  * @return <Array> game
+  * @return <String> mainimage
+  * @return <Array> images
+  */
   gameAction: function() {
+    'use strict';
     var _this = this;
     this.mongodb
     .collection('missingparts_games')
@@ -32,29 +44,40 @@ module.exports.prototype = GamesController.prototype.extend({
     .nextObject(function(err, game) {
       _this.renderGame(game, function(err, images){
         _this.view.render({
-          title: "Fehlstellen-Spiel",
+          title: 'Fehlstellen-Spiel',
           game: game,
           mainimage: game.image,
           images: images
-          });
+        });
       });
     });
   },
 
-  // gathers images from mongodb
+  /**
+  *  renderGame() fetches images from mongodb
+  *
+  * @param <Array> game
+  * @param <Callback> renderCallback
+  * @return <Array> images in Callback //FIXME: what is the type of a callback?
+  */
   renderGame: function(game, renderCallback) {
+    'use strict';
     var type = game.category;
+
     // filter buildings by category
     if (type) {
+
+        // TODO: add random and limit count of images?
       this.mongodb
         .collection('missingparts_images')
         .find({category: type}) //, _random: {$near: [Math.random(), 0]}})
         .toArray(renderCallback);
-        // TODO: add random and limit count of images?
     }
     else {
+
       // should never happen
-      console.log("[FAIL]: missing game.category for missingparts game.id: " + game._id + " - \ncheck the database for corrupt or missing data.");
+      console.log('[FAIL]: missing game.category for missingparts game.id: ' +
+       game._id + ' - \ncheck the database for corrupt or missing data.');
       this.mongodb
         .collection('missingparts_images')
         .find({_random: {$near: [Math.random(), 0]}})
@@ -62,17 +85,24 @@ module.exports.prototype = GamesController.prototype.extend({
     }
   },
   
-  // checks if game selection is correct
-  checkSelectedAction: function(res, req) {
-    var _this = this;
-    var result = this.request.param('result');
-    var selected = _this.request.param('selects');
-    var gameid = this.request.param('gameid');
+  /**
+  * checkSelectedAction() checks if game selection is correct
+  *
+  * @param <Number> result
+  * @param <String> gameid
+  * @return <Boolean> correct
+  * @return <Number> correctBuilding
+  */
+  checkSelectedAction: function() {
+    'use strict';
+    var _this = this,
+     result = _this.request.param('result'),
+     gameid = _this.request.param('gameid');
 
     // get the game paramenters first
-    this.mongodb
+    _this.mongodb
     .collection('missingparts_games')
-    .find( {_id: this.mongo.ObjectID(gameid)} )
+    .find( {_id: _this.mongo.ObjectID(gameid)} )
     .nextObject(function(err, game) {
 
       // get the image data now
@@ -88,20 +118,24 @@ module.exports.prototype = GamesController.prototype.extend({
 
           // TODO: we should not compare string names
           // TODO: the db should have a reference to the img id
-          if(images[i].title === game.correctpart) {
-            if(result === i) {
+
+          // parse Strings and Numbers to prevent bugs
+          // and check them for equality
+          if(String(images[i].title) === String(game.correctpart)) {
+            if(Number(result) === Number(i)) {
               correctImageSelected = true;
             }
             correctImageNumber = i;
           }
-        };
+        }
 
         // send the solution back to client
+
         _this.response.json( {
           correct: correctImageSelected,
           correctBuilding: correctImageNumber
         });
-      })
-    })
+      });
+    });
   }
 });
