827bf014944278b145016c694e6824f51a8e69ec
mansur <mansuriqbal@gmail.com>
2014-02-04 23:19:29 +0100

    image caching now possible. new ressources params: size=middle or size=large

--- controllers/index/Ressources.js
+++ controllers/index/Ressources.js
@@ -9,47 +9,104 @@ module.exports.prototype = BaseController.prototype.extend({
 
   imageAction: function() {
     var _this = this;
-    if (this.request.param('id')) {
+    if (_this.request.param('id')) {
       // if comma seperated ids are given, take random pic from it
-      var picIds = this.request.param('id').split(','),
-        picId = picIds[Math.floor(Math.random()*picIds.length)];
-      this.mongodb
-        .collection('images')
-        .find({_id: this.mongo.ObjectID(picId)})
-        .nextObject(function(err, doc) {
-          if (doc) {
+      var picIds = _this.request.param('id').split(','),
+        picId = picIds[Math.floor(Math.random()*picIds.length)],
+        size = _this.request.param('size');
 
-            _this.response.type('image/' + doc.type);
+      if (size !== 'middle' && size !== 'large') {
+        size = '';
+      }
+      else {
+        size = '_' + size;
+      }
 
-            var width = _this.request.param('width');
-            if (width) {
-              var pic = gm(doc.data.read(0,doc.data.length()));
-              pic.resize(width).toBuffer(function (err, buffer) {
-                if (err)
-                  _this.response.send(404, 'Image not found');
-                _this.response.send(buffer);
+      var notFoundFunc = function() {
+        _this.response.send(404, 'Image not found');
+      },
+
+      showImageFunc = function(type, buffer) {
+        _this.response.type('image/' + type);
+        _this.response.send(buffer);
+      },
+
+      resizeAndShowImageFunc = function(picId) {
+        _this.mongodb
+          .collection('images')
+          .find({_id: _this.mongo.ObjectID(picId)})
+          .nextObject(function(err, doc) {
+            // getting original image
+            if (doc) {
+              var width,
+                pic = gm(doc.data.read(0,doc.data.length()));
+
+              // predefined widths for sizes
+              if (size === '_middle') width = 200;
+              else if (size === '_large') width = 1024;
+
+              // resize and get buffer
+              pic.resize(width).size(function(err, picSize) {
+                pic.toBuffer(function (err, buffer) {
+                  if (err) {
+                    notFoundFunc();
+                  }
+                  else {
+                    doc.data = _this.request.mongo.Binary(buffer);
+                    doc.width = picSize.width;
+                    doc.height = picSize.height;
+                    // write new image into mongodb
+                    _this.mongodb
+                      .collection('images' + size)
+                      .insert(doc, {}, function(err, newDoc) {
+
+                      });
+                    showImageFunc(doc.type, buffer);
+                    //_this.response.type('image/' + doc.type);
+                    //_this.response.send(buffer);
+                  }
+                });
               });
             }
             else {
-              _this.response.send(doc.data.value(true));
+              notFoundFunc();
             }
-            /*
-            var pic = gm(doc.data.read(0,doc.data.length()));
-            pic.size(function(err, size){
-              pic.format(function(err, format){
-                console.log(size, format);
-              });
-            });
-            */
+          });
+      };
+
+      // find resized image
+      _this.mongodb
+        .collection('images' + size)
+        .find({_id: _this.mongo.ObjectID(picId)})
+        .nextObject(function(err, doc) {
+          if (doc) {
+            // if found render
+            showImageFunc(doc.type, doc.data.value(true));
           }
           else {
-            _this.response.send(404, 'Image not found');
+            if (size) {
+              // else we have to resize and save the image
+              resizeAndShowImageFunc(picId);
+            }
+            else {
+              notFoundFunc();
+            }
           }
         });
     }
     else {
-      _this.response.send(404, 'Image not found');
+      _this.response.send(404, 'wrong parameter');
     }
   }
 });
+
+
+/*
+var pic = gm(doc.data.read(0,doc.data.length()));
+pic.size(function(err, size){
+  pic.format(function(err, format){
+    console.log(size, format);
+  });
+});
+*/
 
