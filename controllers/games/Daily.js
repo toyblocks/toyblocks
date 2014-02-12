eda8d052688b48fd4290c3be5b2e9e8c16950cdd
Steven Reynolds <reynolds.tud@gmail.com>
2014-02-12 23:50:13 +0100

    SublimeLinter fixes

--- controllers/games/Daily.js
+++ controllers/games/Daily.js
@@ -1,7 +1,6 @@
 'use strict';
 
-var GamesController = require('../Games'),
-attributeModel = require('../../models/Attribute');
+var GamesController = require('../Games');
 
 module.exports = function () {
 
@@ -14,7 +13,8 @@ module.exports.prototype = GamesController.prototype.extend({
   //
   // @return percentGamesPlayed
   getPercentGamesPlayed: function(){
-    // TODO currentGamesPlayed = this.mongodb.collection('players').find( hasPlayedDaily: true).count()
+    // TODO currentGamesPlayed = this.mongodb.collection('players')
+    //      .find( hasPlayedDaily: true).count()
     // TODO maxGamesPlayed = this.mongodb.collection('players').count()
 
     var currentGamesPlayed = 120,
@@ -40,13 +40,41 @@ module.exports.prototype = GamesController.prototype.extend({
     var _this = this;
 
     //TODO: get proper player data from mongodb
-    var players2 = [];
+    //var players2 = [];
     var players = [{
-      playerid: 1, pos: 1, name: 'FayeValentine', score: 1337, logo: 'none'},{
-      playerid: 2, pos: 2, name: 'Edward', score: 1200, logo: 'none'}, {
-      playerid: 5, pos: 3, name: 'SpikeSpiegel', score: 1111, logo: 'none'},{
-      playerid: 4, pos: 4, name: 'Ein', score: 734, logo: 'none'},{
-      playerid: 3, pos: 5, name: 'Jet_Black', score: 234, logo: 'none'
+      playerid: 1,
+      pos: 1,
+      name: 'FayeValentine',
+      score: 1337,
+      logo: 'none'
+    },
+       {
+      playerid: 2,
+      pos: 2,
+      name: 'Edward',
+      score: 1200,
+      logo: 'none'
+    },
+      {
+      playerid: 5,
+      pos: 3,
+      name: 'SpikeSpiegel',
+      score: 1111,
+      logo: 'none'
+    },
+       {
+      playerid: 4,
+      pos: 4,
+      name: 'Ein',
+      score: 734,
+      logo: 'none'
+    },
+       {
+      playerid: 3,
+      pos: 5,
+      name: 'Jet_Black',
+      score: 234,
+      logo: 'none'
     }];
 
     // TODO: get date from dailychallenge database
@@ -59,11 +87,11 @@ module.exports.prototype = GamesController.prototype.extend({
     };
 
     _this.view.render({
-      title: "DailyChallenge",
+      title: 'DailyChallenge',
       game: game,
       players: players,
       userid: 2
-    })
+    });
   },
 
   // Timed function that generates every day a new set of games
@@ -75,17 +103,24 @@ module.exports.prototype = GamesController.prototype.extend({
     // TODO: set hasPlayedDaily for all players to false
     // TODO: also insert date to mongodb
     // TODO: create a new leaderboard
-    console.log("generating Daily Game")
-    var missing = _this.mongodb.collection('missingparts_games').find({}).toArray(function(err, data) {
+    console.log('generating Daily Game');
+
+    _this.mongodb
+    .collection('missingparts_games')
+    .find({})
+    .toArray(function(err, data) {
       for (var i = data.length - 1; i >= 0; i--) {
-        console.log("missingparts_games - " + data[i].title);
-      };
+        console.log('missingparts_games - ' + data[i].title);
+      }
     });
 
-    sorting = _this.mongodb.collection('sorting_games').find().toArray(function(err, data) {
+    _this.mongodb
+    .collection('sorting_games')
+    .find()
+    .toArray(function(err, data) {
       for (var i = data.length - 1; i >= 0; i--) {
-        console.log("sorting_games - " + data[i].title);
-      };
+        console.log('sorting_games - ' + data[i].title);
+      }
     });
   },
 
@@ -95,38 +130,44 @@ module.exports.prototype = GamesController.prototype.extend({
   gameAction: function() {
     var _this = this;
 
-    // TODO: create a db entry for daily_games, let generateDailyGame create a game and send that entry back to the client
+    // TODO: create a db entry for daily_games
+    // let generateDailyGame create a game and send the entry back to client
     // TODO: figure out how to make multiple games on clientside work
 
-    // this counts the available games, takes a random of each and sends it back to the client
+    // this counts the available games
+    // takes a random of each and sends it back to the client
     //_this.generateDailyGame();
     var games;
-    _this.mongodb.collection('missingparts_games').find().count(function (e, count1) {
+    _this.mongodb.collection('missingparts_games')
+    .find().count(function (e, count1) {
       var random = Math.random() * count1;
-      _this.mongodb.collection('missingparts_games').find().limit(-1).skip(random).next(function (err, ele1) {
-        _this.mongodb.collection('sorting_games').find().count(function (e, count2) {
+      _this.mongodb.collection('missingparts_games')
+      .find().limit(-1).skip(random).next(function (err, ele1) {
+        _this.mongodb.collection('sorting_games')
+        .find().count(function (e, count2) {
           random = Math.random() * count2;
-          _this.mongodb.collection('sorting_games').find().limit(-1).skip(random).next(function (err, ele2) {
+          _this.mongodb.collection('sorting_games')
+          .find().limit(-1).skip(random).next(function (err, ele2) {
             games = [{
               id: ele1._id,
-              type: "missing",
+              type: 'missing',
               title: ele1.title
             },{
               id: ele2._id,
-              type: "sorting",
+              type: 'sorting',
               title: ele2.title
             }];
 
             // log debug info
             console.log(ele1);
-            console.log("games");
+            console.log('games');
             console.log(games);
 
             // send data back to client
             _this.view.render({
-              title: "DailyChallenge",
+              title: 'DailyChallenge',
               games: games
-            })
+            });
           });
         });
       });
