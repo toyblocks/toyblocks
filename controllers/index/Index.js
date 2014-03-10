9c131f25a7caa2609659b98d2cb97adb10861d6b
Steven Reynolds <reynolds.tud@gmail.com>
2014-03-10 15:16:42 +0100

    #9181: last updated in footer

--- controllers/index/Index.js
+++ controllers/index/Index.js
@@ -39,5 +39,23 @@ module.exports.prototype = BaseController.prototype.extend({
       title: '404 Seite nicht gefunden',
       route: '/'
     });
+  },
+
+  lastupdateAction: function() {
+    var _this = this;
+    function fill(n, length) {
+      var str = '' + n;
+      for (var i = str.length+1; i <= length; i++) {
+          str = '0' + str;
+      }
+      return str;
+    }
+    var date = new Date();
+    var string = date.getFullYear()         + "-" + 
+                fill((date.getMonth()+1),2) + "-" +
+                fill( date.getDate()    ,2) + " " +
+                fill( date.getHours()   ,2) + ":" +
+                fill( date.getMinutes() ,2) + "\n";
+    _this.response.json({lastupdate:string});
   }
 });
