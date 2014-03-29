ac5658373a246c7d40ac0f2c812b5534aa55107e
Steven Reynolds <reynolds.tud@gmail.com>
2014-03-29 19:50:03 +0100

    Daily leaderboard db

--- controllers/games/Daily.js
+++ controllers/games/Daily.js
@@ -9,103 +9,121 @@ module.exports = function () {
 module.exports.prototype = GamesController.prototype.extend({
   name: 'daily',
 
-  // Calculates how many people played todays Daily
-  //
-  // @return percentGamesPlayed
+  /* Calculates how many people played todays Daily
+  *@return percentGamesPlayed
+  */ 
   getPercentGamesPlayed: function(){
     var _this = this;
-    _this.mongodb.collection('users').find().count(function (err, maxGamesPlayed) {
-      _this.mongodb.collection('users').find({hasPlayedDaily: true}).count(function (err, currentGamesPlayed) {
-      return ((currentGamesPlayed * 100) / maxGamesPlayed);;
+
+    _this.mongodb
+     .collection('users')
+     .find({right_level: 300})
+     .count(function (err, maxGamesPlayed) {
+        maxGamesPlayed = maxGamesPlayed || 1;
+      _this.mongodb
+       .collection('daily_leaderboard')
+       .find()
+       .count(function (err, currentGamesPlayed) {
+        currentGamesPlayed = currentGamesPlayed || 0;
+        console.log(currentGamesPlayed, maxGamesPlayed);
+        return ((currentGamesPlayed * 100) / maxGamesPlayed);;
       });
     });
   },
 
-  // GET index Page for DailyChallenge
+  /**
+  * GET index Page for DailyChallenge
+  */
   indexAction: function() {
     var _this = this,
-      userId  = _this.request.session.user.tuid;
+      userId  = _this.request.session.user.tuid,
+      played = _this.getPercentGamesPlayed();
+
     console.log(userId);
-    _this.mongodb.collection('users').find({tuid: userId}).nextObject(function (err, user) {
-      console.log(user);
-      var hasPlayed = false; //user.hasPlayedDaily || false;
+    _this.mongodb
+     .collection('daily_leaderboard')
+     .find({tuid: userId})
+     .nextObject(function (err, user) {
+      
+      
+      var hasPlayed = (typeof user === null);
+      console.log(hasPlayed, userId, played);
+
       _this.view.render( {
         title: 'Daily Challenge',
-        gamesPlayed: _this.getPercentGamesPlayed(),
+        gamesPlayed: played,
         hasPlayedTodaysDaily: hasPlayed
       });
     });
   },
 
-
   // GET leaderboard webpage
   leaderboardAction: function () {
     var _this = this;
 
-/*
-    _this.mongodb.collection('users')
-    .find() // {playedDaily: true }
+    _this.mongodb
+    .collection('daily_leaderboard')
+    .find()
+    .sort({score: 1})
+    .limit(15)
     .toArray(function (err, users) {
-      for (var i = 0; i < users.length; i++) {
-        users[i]
+      /*if(typeof users === 'undefined'){
+        users = [{
+          playerid: 1,
+          pos: 1,
+          name: 'FayeValentine',
+          score: 1337,
+          logo: 'none'
+        },
+           {
+          playerid: 2,
+          pos: 2,
+          name: 'Edward',
+          score: 1200,
+          logo: 'none'
+        },
+          {
+          playerid: 5,
+          pos: 3,
+          name: 'SpikeSpiegel',
+          score: 1111,
+          logo: 'none'
+        },
+           {
+          playerid: 4,
+          pos: 4,
+          name: 'Ein',
+          score: 734,
+          logo: 'none'
+        },
+           {
+          playerid: 3,
+          pos: 5,
+          name: 'Jet_Black',
+          score: 234,
+          logo: 'none'
+        }];*/
+        
+      var d = new Date();
+      var game = {
+        year: d.getFullYear(),
+        month: d.getMonth()+1,
+        day: d.getDate(),
+        gamesPlayed: _this.getPercentGamesPlayed()
       };
-    });
-*/
-    var players = [{
-      playerid: 1,
-      pos: 1,
-      name: 'FayeValentine',
-      score: 1337,
-      logo: 'none'
-    },
-       {
-      playerid: 2,
-      pos: 2,
-      name: 'Edward',
-      score: 1200,
-      logo: 'none'
-    },
-      {
-      playerid: 5,
-      pos: 3,
-      name: 'SpikeSpiegel',
-      score: 1111,
-      logo: 'none'
-    },
-       {
-      playerid: 4,
-      pos: 4,
-      name: 'Ein',
-      score: 734,
-      logo: 'none'
-    },
-       {
-      playerid: 3,
-      pos: 5,
-      name: 'Jet_Black',
-      score: 234,
-      logo: 'none'
-    }];
 
-    // TODO: get date from dailychallenge database
-    var d = new Date();
-    var game = {
-      year: d.getFullYear(),
-      month: d.getMonth()+1,
-      day: d.getDate(),
-      gamesPlayed: _this.getPercentGamesPlayed()
-    };
-
-    _this.view.render({
-      title: 'DailyChallenge',
-      game: game,
-      players: players,
-      userid: 2
+      _this.view.render({
+        title: 'Daily Challenge Bestenliste',
+        game: game,
+        players: users,
+        userid: _this.request.session.user.tuid
+      });
     });
   },
 
-  // Timed function that generates every day a new set of games
-  //
+  /**
+  *  Timed function that generates every day a new set of games
+  */
   generateDailyGame: function() {
     var _this = this;
 
@@ -134,12 +152,12 @@ module.exports.prototype = GamesController.prototype.extend({
     });
   },
 
-  // GET daily game
-  //
-  // @return games - list of games
+  /* GET daily game
+  *  @return games - list of games
+  */ 
   gameAction: function() {
     var _this = this;
-
+    var tuid = _this.request.session.user.tuid;
     // TODO: create a db entry for daily_games
     // let generateDailyGame create a game and send the entry back to client
     // TODO: figure out how to make multiple games on clientside work
@@ -153,13 +171,27 @@ module.exports.prototype = GamesController.prototype.extend({
       assemble: ['52f171d934e48b00006e0070'],
       multiplechoice: ['52f2aecb029240e1a4000008,5322314a855748ea73661eab,52f2af7b029240e1a400000a']
     };
-    _this.view.render({
-      title: 'Daily Challenge',
-      games: games,
-      missing: games.missing,
-      sorting: games.sorting,
-      assemble: games.assemble,
-      multiplechoice: games.multiplechoice
+
+    _this.mongodb
+    .collection('daily_leaderboard')
+    .find({tuid: tuid})
+    .nextObject(function (err, ele) {
+      console.log(ele);
+      if(!!ele){
+        _this.view.render({
+          title: 'Daily Challenge',
+          error: 'Sie haben bereits das heutige Daily gespielt!'
+        });
+      }else{
+        _this.view.render({
+          title: 'Daily Challenge',
+          games: games,
+          missing: games.missing,
+          sorting: games.sorting,
+          assemble: games.assemble,
+          multiplechoice: games.multiplechoice
+        });
+      }
     });
   },
 
@@ -193,80 +225,45 @@ module.exports.prototype = GamesController.prototype.extend({
     .collection('daily_leaderboard')
     .find({tuid: tuid})
     .nextObject(function (err, ele) {
-      if(typeof ele !== 'undefined'){
+      /*
+      if(!!ele){
+
+
+      }else{
         _this.view.render({
           error: 'Error: Sie haben das Daily heute schon gespielt oder der Server macht Mist.'
-        })
-      }
-      _this.mongodb
-      .collection('daily_leaderboard')
-      .update({tuid: tuid},
-            { tuid: tuid,
-              name: _this.request.session.user.nickname,
-              score: points
-             },{}, function (err) {
-              if(err) console.log(err);
-            });
-      _this.mongodb
-      .collection('daily_leaderboard')
-      .find()
-      .sort({score: 1})
-      .limit(15)
-      .toArray(function (err, ele) {
-        var d = new Date();
-        var game = {
-          year: d.getFullYear(),
-          month: d.getMonth()+1,
-          day: d.getDate(),
-          gamesPlayed: _this.getPercentGamesPlayed()
-        };
-
-        _this.view.render({
-          title: 'Daily Challenge',
-          result: result,
-          game: game,
-          players: players,
-          userid: _this.request.session.user.tuid
         });
-      });
+      }
+      */
+      _this.mongodb
+      .collection('daily_leaderboard')
+      .insert({ tuid: tuid,
+              nickname: _this.request.session.user.tuid,
+              score: points
+        }, function (err, ele) {
+          _this.mongodb
+          .collection('daily_leaderboard')
+          .find()
+          .sort({score: 1})
+          .limit(15)
+          .toArray(function (err, players) {
+            var d = new Date();
+            var game = {
+              year: d.getFullYear(),
+              month: d.getMonth()+1,
+              day: d.getDate(),
+              gamesPlayed: _this.getPercentGamesPlayed()
+            };
+
+            _this.view.render({
+              title: 'Daily Challenge',
+              result: result,
+              game: game,
+              players: players,
+              userid: _this.request.session.user.tuid
+            });
+          });    
+        });
     });
-    /*
-    // TODO: Push result to db
-    var players = [{
-      tuid: 1,
-      name: 'FayeValentine',
-      score: 1337,
-      logo: 'none'
-    },
-       {
-      tuid: 2,
-      name: 'Edward',
-      score: 1200,
-      logo: 'none'
-    },
-      {
-      tuid: 5,
-      name: 'SpikeSpiegel',
-      score: 1111,
-      logo: 'none'
-    },
-       {
-      tuid: 4,
-      name: 'Ein',
-      score: 734,
-      logo: 'none'
-    },
-       {
-      tuid: 3,
-      name: 'Jet_Black',
-      score: 234,
-      logo: 'none'
-    },{
-      tuid: _this.request.session.user.tuid,
-      name: _this.request.session.user.surname,
-      score: points,
-      logo: 'none'
-    }];
-*/
   }
 });
