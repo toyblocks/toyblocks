438d91d8d322a9695d0a334da7c6557e07b502d9
Steven Reynolds <reynolds.tud@gmail.com>
2014-04-02 02:14:08 +0200

    Daily now every day at 00:01

--- jobs.js
+++ jobs.js
@@ -7,36 +7,9 @@ var cronJob = require('cron').CronJob,
 
 
 module.exports.initJobs = function initJobs (mongodb) {
-  // run every monday at 5am
-  new cronJob('0 5 * * 1', function() {
-    console.log('Die Woche hat begonnen und es ist 5 Uhr morgens.');
-    
-  }, null, true);
 
-  daily.generateDailyGame(mongodb);
-  // TODO: move generateDailyGame to daily at 5am
-  // run every hour at 25 min
-  new cronJob('0 * * * *', function() {
+  // start the script daily at 00:01
+  new cronJob('1 0 * * *', function() {
     daily.generateDailyGame(mongodb);
   }, null, true);  
-
-  // TODO: move generateDailyGame to daily at 5am
-  // run every hour at 25 min
-  new cronJob('15 * * * *', function() {
-    daily.generateDailyGame(mongodb);
-  }, null, true);  
-
-
-  // TODO: move generateDailyGame to daily at 5am
-  // run every hour at 25 min
-  new cronJob('30 * * * *', function() {
-    daily.generateDailyGame(mongodb);
-  }, null, true);
-
-
-  // TODO: move generateDailyGame to daily at 5am
-  // run every hour at 25 min
-  new cronJob('45 * * * *', function() {
-    daily.generateDailyGame(mongodb);
-  }, null, true);
 }
