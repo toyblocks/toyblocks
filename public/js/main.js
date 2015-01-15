a5cb7a340a6d643af035e284eb1e9c8b104fcc58
Steven Reynolds <reynolds.tud@gmail.com>
2015-01-15 13:53:40 +0100

    moved summernode javascript to admin.js

--- public/js/main.js
+++ public/js/main.js
@@ -131,28 +131,7 @@ $(function(){
     }
   });
 
-  var initSummernote = function() {
-    $('.summernote').not('.loaded').summernote({
-      disableDragAndDrop: true,
-      height: 200,
-      toolbar: [
-        ['style', ['style']],
-        ['font', ['bold', 'italic', 'underline', 'clear']],
-        /* ['fontname', ['fontname']], */
-        /* ['fontsize', ['fontsize']], */
-        ['color', ['color']],
-        ['para', ['ul', 'ol', 'paragraph']],
-        /* ['height', ['height']], */
-        ['table', ['table']],
-        ['insert', ['link', 'picture', 'video']],
-        ['view', ['fullscreen', 'codeview']],
-        ['help', ['help']]
-      ]
-    }).addClass('loaded');
-  };
-
   $(document).ready(function(){
-    initSummernote();
 
     /* object references selection for objects */
     var $objecttypeTrigger;
